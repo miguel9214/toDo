f4d0e65bba67dec857a218ce1f1e5d8054064a28
miguel9214 <40032128+miguel9214@users.noreply.github.com>
2022-11-26 22:27:40 -0500

    Navbar con errores

--- app/src/Layout.jsx
+++ app/src/Layout.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Listtask from './Listtask/Listtask'
-import Mainpage from './Mainpage/Mainpage'
-import Registertask from './Registertask/Registertask'
+// import Listtask from './Listtask/Listtask'
+// import Mainpage from './Mainpage/Mainpage'
+// import Registertask from './Registertask/Registertask'
+import NavBar from './NavBar/NavBar'
 
 const Layout = () => {
   return (
     <>
-     <Mainpage/>
+    <NavBar/>
+     {/* <Mainpage/>
      <Registertask/>
-     <Listtask/>
+     <Listtask/> */}
     </>
    
     
